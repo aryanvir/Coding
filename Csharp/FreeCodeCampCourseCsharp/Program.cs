3394cdaad065022f361128bd6e2000cf8b11e1c9
aryan vir <63742316+aryanvir@users.noreply.github.com>
2021-11-23 15:46:51 +0530

    Add files via upload

---
+++ Csharp/FreeCodeCampCourseCsharp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+
+
+namespace FreeCodeCampCourseCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Chef chef = new Chef();
+           chef.MakeChiken();
+           ItalianChef italianChef = new ItalianChef();
+            italianChef.MakeChiken();
+            italianChef.MakePasta();
+            italianChef.MakespecialDish();
+
+
+        }
+
+    }
+
+}
