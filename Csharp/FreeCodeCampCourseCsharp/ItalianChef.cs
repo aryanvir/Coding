96111dbae907fc9f890076650c2c6990294ff463
aryan vir <63742316+aryanvir@users.noreply.github.com>
2021-11-23 15:45:55 +0530

    Add files via upload

---
+++ Csharp/FreeCodeCampCourseCsharp/ItalianChef.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreeCodeCampCourseCsharp
+{
+    class ItalianChef : Chef
+    {
+       public void MakePasta()
+        {
+            Console.WriteLine("Made Pasta");
+
+        }
+        public override void MakespecialDish()
+        {
+            Console.WriteLine("The chef made chicken parm ");
+        }
+    }
+}
