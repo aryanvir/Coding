7882700a26e2d861b31537dacd9fe21347d7aa30
aryan vir <63742316+aryanvir@users.noreply.github.com>
2021-11-23 16:01:07 +0530

    Add files via upload

---
+++ Csharp/CsharpCode/Chef.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreeCodeCampCourseCsharp
+{
+    internal class Chef
+    {
+        public void MakeChiken()
+        {
+            Console.WriteLine("The Chef made chicken ");
+
+        }
+
+        public void MakeSalad()
+        {
+            Console.WriteLine("The chef makes salad");
+        }
+
+        public virtual void MakespecialDish()
+        {
+            Console.WriteLine("The chef made BBQ Spicy Chicken ");
+        }
+
+    }
+}
---
+++ Csharp/CsharpCode/Movie.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreeCodeCampCourseCsharp
+{
+    internal class Movie
+    {
+        public string title;
+        public string director;
+        private string rating; // Ratings should only be G, PG, PG-13, R, NR
+        // therefore the rating is private which will not allow other program to acces it 
+
+        public Movie(string movieTitle, string movieDirector, string movieRating)
+        {
+            title = movieTitle;
+            director = movieDirector;
+            Rating  = movieRating;
+
+        }
+        public string Rating 
+        { 
+            get { return rating; }
+            set { 
+                if (value == "G" || value == "PG" || value == "PG-13" || value == "R" || value == "NR")
+                {
+                    rating = value;
+
+                }
+                else
+                {
+                    rating = "NR";
+                }; 
+            } 
+        }
+    }
+}
