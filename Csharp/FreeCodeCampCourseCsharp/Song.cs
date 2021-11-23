e1371bfa1141259ba1f8daf3862bbc9f24522159
aryan vir <63742316+aryanvir@users.noreply.github.com>
2021-11-23 15:47:17 +0530

    Add files via upload

---
+++ Csharp/FreeCodeCampCourseCsharp/Song.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreeCodeCampCourseCsharp
+{
+    internal class Song
+    {
+        public string title;
+        public string artist;
+        public int duration; // An attrubute to the object of the class 
+        public static int songCount = 0; // An atribute to the class 
+
+        public Song(string songTitle,string songArtist, int songDuration)
+        {
+            title = songTitle;
+            artist = songArtist;
+            duration = songDuration;
+            songCount++;
+
+        }
+    }
+}
---
+++ Csharp/FreeCodeCampCourseCsharp/Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreeCodeCampCourseCsharp
+{
+    internal class Student
+    {
+        public string name;
+        public string major;
+        public double gpa;
+
+        public Student(string studentName, string studentMajor, double studentGpa)
+        {
+            name = studentName;
+            major = studentMajor;
+            gpa = studentGpa;
+
+        }
+
+        public bool hasHonors()
+        {
+            if(gpa >= 3.5)
+            {
+                return true;
+            }
+            return false;
+        }
+
+
+    }
+}
